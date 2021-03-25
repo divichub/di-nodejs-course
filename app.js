53def08c72eeb81f03a37ff09cc27d85dd0913ec
Deniel Ivic <d.ivic@reply.de>
2021-03-25 15:59:27 +0100

    initial commit

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Users page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Home page</h1>");
+});
+
 app.listen(9000);
