8086e203aedb179df7cfee2c9d8374a56a017c32
Deniel Ivic <d.ivic@reply.de>
2021-04-22 11:20:29 +0200

    end section

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
-exports.get404 = (req, res) => {
-  res.status(404).render("404", { pageTitle: "404 - Not Found", path: "/" });
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
